4eb73e7c0eed49b0bcf33301c5f2a8146fa723dc
Dennis Hecker <dennis@Denniss-MacBook-Pro.local>
2019-12-04 09:56:22 +0100

    with template tags

---
+++ draftEpi2/src/remQuery/templatetags/rem_template_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_cellType_attr(obj, cellType, is_activity_or_samplecount):
+    if is_activity_or_samplecount == 'activity':
+        attr = obj[cellType+'_dnase1Log2']
+    if is_activity_or_samplecount == 'samplecount':
+        attr = obj[cellType+'_samplecount']
+    return attr
