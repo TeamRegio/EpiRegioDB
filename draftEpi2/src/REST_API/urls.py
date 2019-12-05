eb96e221e98120fe12b2c0de592edf3c20e6f129
BaumgartenNina <n.baumgarten@em.uni-frankfurt.de>
2019-12-05 12:13:17 +0100

    chances url

--- draftEpi2/src/REST_API/urls.py
+++ draftEpi2/src/REST_API/urls.py
@@ -3,12 +3,12 @@ from . import views #import own view file
 
 urlpatterns = [
 	# display all genes: /REST_API/GeneInfo/
-	url(r'^GeneInfo/', views.GenesInfo.as_view(), name = "genes_info"),
+#	url(r'^GeneInfo/$', views.GenesInfo.as_view(), name = "genes_info"),
 	#display specific gene:  /REST_API/GeneInfo/GeneId/
 	url(r'^GeneInfo/(?P<gene_id>[A-Z0-9]+)/$', views.GeneInfo.as_view(), name = "gene_info"),
 
 	#show all REMs: /REST_API/REMs/
-	url(r'^REMs/', views.REMsInfo.as_view(), name = "REMs_info"),
+#	url(r'^REMs/', views.REMsInfo.as_view(), name = "REMs_info"),
 
 	#show specific REM: /REST_API/REMs/REMID/
 	url(r'^REMs/(?P<REM_id>[A-Z0-9]+)/$', views.REMInfo.as_view(), name = "REM_info"),
