347856514c9c6f3375287f9d3fdfc5752d70e500
DennisHeck <55958437+DennisHeck@users.noreply.github.com>
2019-12-06 11:14:55 +0100

    Update API.py
    all query functions, including comments on their functionality

--- draftEpi2/src/API.py
+++ draftEpi2/src/API.py
@@ -64,21 +64,36 @@ if __name__ == "__main__":
 	__main__()
 
 
-def API_CellTypesActivity(REM, cellTypes_list):
-	REMID = REM['REMID']
+"""
+All query functions do a first filter step to give back the fitting data. Next the functions to filter
+for cellTypes (if any cellTypes given) and for adding the CREM information are called. The additional information is
+added to the dictionaries.
+"""
 
+
+
+# For every query, we give the possibility to give back the activity of the REMs. This function is only
+# called, when there are cellTypes given. Another option is to provide a threshold, which excludes all the REMs that
+# have an activity below it. For more than one cellType a REM's activity has to exceed in ALL of them.
+# If no threshold was set, the view returns a 0 as threshold.
+def API_CellTypesActivity(REM, cellTypes_list, activ_thresh):
+
+	REMID = REM['REMID']
 	for cellType in cellTypes_list:
-		matching_samples = REMActivity.objects.filter(REMID=REMID).filter(sampleID__cellTypeID__cellTypeName=cellType).values()
+		matching_samples = REMActivity.objects.filter(REMID=REMID).filter(sampleID__cellTypeID__cellTypeName=cellType).filter(dnase1Log2__gte=activ_thresh).values()
 		activity = 0
-		for sample in matching_samples:
-			activity += sample['dnase1Log2']
-		mean_activity = activity/len(matching_samples)
-		REM[cellType + '_dnase1Log2'] = mean_activity
-		REM[cellType + '_samplecount'] = len(matching_samples)
-		# print(REM)
+		if len(matching_samples) > 0:  # as it's possible that no REM is remaining
+			for sample in matching_samples:
+				activity += sample['dnase1Log2']
+			mean_activity = activity/len(matching_samples)
+			REM[cellType + '_dnase1Log2'] = mean_activity
+			REM[cellType + '_samplecount'] = len(matching_samples)
+		else:
+			return  # we return None if there is no match. The Nones are filtered out in the origin function
 	return REM
 
-#TODO: maybe add one line that explain the functionality of each method
+
+# Giving back the additional information about the CREM that the REMs are belonging to
 def API_CREM(REM):
 
 	# get the additional columns for the CREMS
@@ -89,27 +104,41 @@ def API_CREM(REM):
 	return REM
 
 
+# This is only for the detail view when clicking on a CREM-link
+def API_CREM_overview(CREMID):
 
-def API_REMID(REMID_list, cellTypes_list):
+	dataset = list(CREMAnnotation.objects.filter(CREMID=CREMID).values(
+		'REMID_id', 'CREMID', 'chr', 'start', 'end', 'REMsPerCREM', 'version', 'REMID_id__geneID', 'REMID_id__start',
+		'REMID_id__end', 'REMID_id__regressionCoefficient', 'REMID_id__pValue'))
+
+	return dataset
+
+
+# The REMID query. Every REM is handled separately.
+def API_REMID(REMID_list, cellTypes_list, activ_thresh):
 
 	hit_list = []
 	for i in REMID_list:
+
 		dataset = REMAnnotation.objects.filter(REMID=i).values()  # .values hands back a queryset containing dictionaries
 		this_rem = dataset[0]  # we get back a queryset, with [0] we get it into a dictionary
-
-		# get the additional columns for the CREMS
-		this_rem = API_CREM(this_rem)
 
 		# if there are cellTypes that should be filtered for, we do it here for every single REM, to directly add it
 		# to the REMs dictionary
 		if len(cellTypes_list) > 0:
-			this_rem = API_CellTypesActivity(this_rem, cellTypes_list)
+				this_rem = API_CellTypesActivity(this_rem, cellTypes_list, activ_thresh)
+
+		# get the additional columns for the CREMS
+		this_rem = API_CREM(this_rem)
 
 		hit_list.append(this_rem)
+		hit_list = [x for x in hit_list if x is not None]  # if a REM's activity in a cell type is under the threshold,
+		# we returned None into the list, now we get rid of it
 
 	return hit_list  # our list of objects, fitting the query_list
 
 
+# If a user provides the geneSymbols, we convert them into ENSG format and use the other functions
 def API_SymbolToENSG(symbol_list):
 
 	hit_list = []
@@ -123,27 +152,55 @@ def API_SymbolToENSG(symbol_list):
 	return hit_list  # our list of objects, fitting the query_list
 
 
-def API_ENSGID(gene_list, cellTypes_list):
+# For the GeneID query: given a set of genes, we look up all the REMs for each of them and add
+# the additional information
+def API_ENSGID(gene_list, cellTypes_list, activ_thresh):
 
 	hit_list = []
+
 	for i in gene_list:
 		dataset = list(REMAnnotation.objects.filter(geneID=i).values())  # we convert the queryset into a list so we can
 		# add values to it
-		if len(cellTypes_list) > 0:
-			for n in range(len(dataset)):
-				dataset[n] = API_CellTypesActivity(dataset[n], cellTypes_list)
-				# dataset[n] = API_CREM(dataset[n])
+		for n in range(len(dataset)):
+			dataset[n] = API_CREM(dataset[n])
+			if len(cellTypes_list) > 0:
+				dataset[n] = API_CellTypesActivity(dataset[n], cellTypes_list, activ_thresh)
 
 		hit_list = hit_list + dataset
+		hit_list = [x for x in hit_list if x is not None]  # if a REM's activity in a cell type is under the threshold,
+		# we returned None into the list, now we get rid of it
+
+	# print(hit_list)
 
 	return hit_list
 
 
+# Is supposed to be used to display the information we have about a gene
 def API_ENSGID_geneInfo(gene_list):
 
-	hit_list=[]
+	hit_list = []
 	for gene in gene_list:
 		dataset = geneAnnotation.objects.filter(geneID=gene).values()
 		hit_list.append(dataset)
 
 	return hit_list
+
+
+# For the GeneRegion query: find all the REMs located in the given regions
+def API_Region(region_list, cellTypes_list, activ_tresh):
+
+	hit_list = []
+
+	for i in region_list:
+		dataset = list(REMAnnotation.objects.filter(chr=i[0]).filter(start__gte=i[1]).filter(end__lte=i[2]).values())
+
+		for n in range(len(dataset)):
+			dataset[n] = API_CREM(dataset[n])
+			if len(cellTypes_list) > 0:
+				dataset[n] = API_CellTypesActivity(dataset[n], cellTypes_list, activ_tresh)
+
+		hit_list = hit_list + dataset
+		hit_list = [x for x in hit_list if x is not None]  # if a REM's activity in a cell type is under the threshold,
+		# we returned None into the list, now we get rid of it
+
+	return hit_list  # our list of dictionaries, fitting the query_list
