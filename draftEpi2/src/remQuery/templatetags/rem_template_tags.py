637011d7e3fc4a339f6b215c88165e778ddd1ddd
Dennis Hecker <dennis@Denniss-MacBook-Pro.local>
2019-12-10 13:59:23 +0100

    updated template tags

--- draftEpi2/src/remQuery/templatetags/rem_template_tags.py
+++ draftEpi2/src/remQuery/templatetags/rem_template_tags.py
@@ -4,6 +4,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_cellType_attr(obj, cellType, is_activity_or_samplecount):
+    cellType = cellType.lower()
     if is_activity_or_samplecount == 'activity':
         attr = round(obj[cellType+'_dnase1Log2'], 5)
     if is_activity_or_samplecount == 'samplecount':
